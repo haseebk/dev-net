71c414fde9dbcb180306fe4cc690f878c923e065
haseeb khan <haseebadilkhan@gmail.com>
2021-02-02 19:49:09 -0700

    implementing middleware for user auth

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+    /**
+     * Obtain token from header
+     */
+    const token = req.header("x-auth-token");
+
+    /**
+     * Account for no token
+     */
+    if (!token) {
+        return res.status(401).json({msg: "No token was found - authorization denied"});
+    } 
+
+    /**
+     * Decode and verify token
+     */
+    try {
+        const decoded = jwt.verify(token, config.get("jwtSecret"));
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({msg: "Invalid token"});
+    }
+
+}
