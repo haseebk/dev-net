0b4f9d903eaca7ad53d95b7d547e6bc7ebca2fd1
haseeb khan <haseebadilkhan@gmail.com>
2021-02-07 22:05:07 -0700

    implementing register page onChange and onSubmit functions

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (  
+  return (
     <nav className="navbar bg-dark">
-        <h1>
-        <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
-    </h1>
-    <ul>
-      <li><a href="#!">Developers</a></li>
-      <li><Link to="/register">Register</Link></li>
-      <li><Link to="/login">Login</Link></li>
-    </ul>
-  </nav>
-    )
-}
+      <h1>
+        <Link to="/">
+          <i className="fas fa-network-wired"></i> DevNet
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <a href="#!">Developers</a>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
