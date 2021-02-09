c6621c5205e3183132d5983664cd11cb18400c95
haseeb khan <haseebadilkhan@gmail.com>
2021-02-08 22:44:04 -0700

    setting up redux and alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -17,9 +20,9 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords don't match");
+      setAlert("Passwords don't match", "danger");
     } else {
-      console.log("Successfully registered");
+      setAlert("Successfully registered", "success");
     }
   };
 
@@ -79,4 +82,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
